1e6a7cf910dac6cbb73a8f669020bf83f296a2c8
Ryan Long <ryanlong1004@gmail.com>
2020-09-04 17:44:52 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/header/Header.jsx"
+import Portfolio from "./components/portfolio/Portfolio.jsx"
+import About from "./components/about/About.jsx"
+import Contact from "./components/contact/Contact.jsx"
+import { Footer, Copyright, ScrollTop } from "./components/footer/Footer.jsx"
+import './styles/main.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Portfolio />
+      <About />
+      <Contact />
+      <Footer />
+      <Copyright />
+      <ScrollTop />
     </div>
   );
 }
