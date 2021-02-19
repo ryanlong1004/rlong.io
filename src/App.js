ef685cae03ada068713d0aef0f8bd5ee92602c23
Ryan Long <ryan@rlong.io>
2021-02-19 01:02:12 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Footer from "./components/Footer"
-import { default as React } from "react";
+import React, { useState } from "react";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Container from "@material-ui/core/Container";
 import "./styles/main.scss";
+import useBookSearch from './useBookSearch';
 
 
 
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
+
+  function handleSearch(e) {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
+  useBookSearch(query, pageNumber)
   return (
     <div className="App">
       <CssBaseline />
       <main>
-        <Hero />
+        <Container maxwidth="sm">
+          <Hero />
+          <input type="text" onChange={handleSearch}></input>
+          <div>Title</div>
+          <div>Title</div>
+          <div>Title</div>
+          <div>Title</div>
+          <div>Loading...</div>
+          <div>Error</div>
+        </Container>
       </main>
       <Footer />
     </div>
