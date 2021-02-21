cd806c771f1723d803294613dce1709d50cf183a
Ryan Long <ryan@rlong.io>
2021-02-21 00:06:13 -0500

    Add Helmet

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import Footer from "./components/Footer"
 import { default as React } from "react";
 import Hero from "./components/Hero";
 import "./styles/main.scss";
+import { Helmet } from "react-helmet";
 
 
 
+
+function todaysDate() {
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  const yyyy = today.getFullYear();
+
+  return `${yyyy}${mm}${dd}`
+}
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Ryan Long - Software Engineer {todaysDate()}</title>
+        <meta name="description" content="Ryan Long is a Software Engineer from the United States with nearly 10 years of experience in Front and Back end technologies." />
+      </Helmet>
       <CssBaseline />
       <main>
         <Hero />
