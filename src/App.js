fd321622723af2adba0bf3f46a7d90a722afd67e
Ryan Long <ryan@rlong.io>
2021-02-21 00:16:55 -0500

    Add Open Graph and Twitter meta tags

--- src/App.js
+++ src/App.js
@@ -24,6 +24,22 @@ function App() {
         <meta charSet="utf-8" />
         <title>Ryan Long - Software Engineer {todaysDate()}</title>
         <meta name="description" content="Ryan Long is a Software Engineer from the United States with nearly 10 years of experience in Front and Back end technologies." />
+        <meta name='robots' content='index, follow' />
+
+
+        {/* Open Graph */}
+        <meta property='og:type' content='article' />
+        <meta property='og:title' content='Ryan Long - Software Engineer' />
+        <meta property='og:description' content='Ryan Long is a Software Engineer from the United States with nearly 10 years of experience in Front and Back end technologies' />
+        <meta property='og:image' content='https://pbs.twimg.com/profile_images/1360428892327186439/rhOQTKuF_400x400.png' />
+        <meta property='og:url' content='rlong.io' />
+        <meta property='og:site_name' content='rlong.io' />
+        {/* Twitter */}
+        <meta name='twitter:title' content='Ryan Long - Software Engineer' />
+        <meta name='twitter:description' content='Ryan Long is a Software Engineer from the United States with nearly 10 years of experience in Front and Back end technologies' />
+        <meta name='twitter:image' content='https://pbs.twimg.com/profile_images/1360428892327186439/rhOQTKuF_400x400.png' />
+        <meta name='twitter:site' content='@ryanrlong' />
+        <meta name='twitter:creator' content='@ryanrlong' />
       </Helmet>
       <CssBaseline />
       <main>
