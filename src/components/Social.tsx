5fcec8692f56f52a94b9349a96c79ffe0b254526
Ryan Long <ryan.long@noaa.gov>
2022-02-15 13:46:35 -0500

    Comment out unused icon

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -4,7 +4,7 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import React from "react";
-import ScheduleIcon from "@material-ui/icons/Schedule";
+// import ScheduleIcon from "@material-ui/icons/Schedule";
 
 // const useStyles = makeStyles((theme) => ({}));
 
